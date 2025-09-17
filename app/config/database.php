cc861ba326fe0f7aa3e9b2d744267d3de41ce624
Cyd05 <nashgeneta@gmail.com>
2025-09-17 11:37:21 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -60,10 +60,10 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'localhost',
-    'port'		=> '',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mockdata',
+    'port'		=> '3306',
+    'username'	=> 'sql12798937',
+    'password'	=> 'yfr3IDm5Ii',
+    'database'	=> 'sql12798937',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
